ac657885a4ae1610690950734b1da0e96753fa38
˗ˏˋJΛYˎˊ <53127946+samrajya-dev@users.noreply.github.com>
2023-12-08 02:01:01 +0530

    Update MyHomeFooter.vue

--- docs/.vuepress/components/MyHomeFooter.vue
+++ docs/.vuepress/components/MyHomeFooter.vue
@@ -2,13 +2,13 @@
   <div class="push"></div>
   <div class="footer center">
     <div>
-      🌸🪷🌷🌼🌻
+      🌸🌸🌸
     </div>
     <div>
       <a href="https://samrajya.dev" target="_blank">SAMRAJYA</a> 🍏 {{ year }}
     </div>
     <div>
-      🔗 <a href="https://linkedin.com/in/samrajya" target="_blank">LinkedIn</a> | <a href="https://github.com/samrajya-dev " target="_blank"></a>
+      🔗 <a href="https://linkedin.com/in/samrajya" target="_blank">LinkedIn</a> | <a href="https://github.com/samrajya-dev " target="_blank">GitHub </a>
     </div>
   </div>
 </template>
